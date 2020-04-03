828af1769ec4e46a0cca89e3a0bfe75a6a40e76c
Jeff Fulmer <jeff@joedog.org>
2020-04-03 12:00:57 -0400

    Added Reinforced and Training

--- src/org/joedog/byron/engine/Engine.java
+++ src/org/joedog/byron/engine/Engine.java
@@ -4,12 +4,16 @@ public abstract class Engine {
   public static final int MENACE     = 0;
   public static final int MINIMAX    = 1;
   public static final int MONTECARLO = 2;
+  public static final int REINFORCED = 3;
+  public static final int TRAINING   = 4;
 
   public Engine () {
   }
 
-  public abstract void reward();
-  public abstract void punish();
-  public abstract void restore();
-  public abstract int  getMove(String pattern); 
+  public abstract void   reward();
+  public abstract void   punish();
+  public abstract void   restore();
+  public abstract void   save();
+  public abstract int    getMove(String pattern); 
+  public abstract String toString();
 }
