35dc989216b773afb8d0f619d72e2863e4429784
Jeff Fulmer <jeff@joedog.org>
2023-05-03 13:26:02 -0400

    Version increment: 1.0.6

--- src/org/joedog/byron/Version.java
+++ src/org/joedog/byron/Version.java
@@ -20,8 +20,8 @@ package org.joedog.pinochle;
  */
 
 public final class Version {
-  public final static String version   = "1.0.5";
-  public final static String copyright = "2011-2020";
+  public final static String version   = "1.0.6";
+  public final static String copyright = "2011-2023";
   public final static String author    = "Jeffrey Fulmer, et al.";
   public final static String email     = "jeff@joedog.org";
 }
